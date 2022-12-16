8efac308726b59932f8bc2f662172cb7552ebdd5
ManelGB <emanuelgb2016@gmail.com>
2022-12-16 20:11:06 -0300

    COrreções atendente view

--- view/AtendenteView.java
+++ view/AtendenteView.java
@@ -1,13 +1,5 @@
 package view;
 
-import java.util.Scanner;
-import models.Anamnese;
-import models.Paciente;
-import models.Sexo;
-import models.Tipo;
-import models.Usuario;
-import models.Validacao;
-
 public class atendenteView {
 
 
